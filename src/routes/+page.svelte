9f283c71b856de5b38268953e00e096495ae8f5d
FIlip Mazur <fmazur677@gmail.com>
2022-12-17 02:39:55 +0100

    post - done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+	import Post from '../components/Post.svelte';
+</script>
+
+<Post />
 <h1 class="text-blue-700 ml-16">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
