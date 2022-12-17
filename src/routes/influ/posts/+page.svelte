58186ab84faa920371ece47ae9e8d01a719aec2a
c-a-v-a <cavaevilmail@gmail.com>
2022-12-17 14:46:27 +0100

    added apply for infu

--- src/routes/influ/posts/+page.svelte
+++ src/routes/influ/posts/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import Post from '$lib/components/Post.svelte';
+	import type { Company, FirebaseResponse } from '$lib/types';
 	const accountIcon = '/account.svg';
 
 	export let data: any;
-	console.log(data);
+	$:item = data.item as FirebaseResponse<Company>
+
 	let influMode = true;
 </script>
 
 <div class="flex w-3/4 md:w-4/5 sm:w-3/4 flex-col">
-	{#each data.item as company}
-		{#each Object.entries(company.posts) as [index, post]}
+	{#each Object.entries(item) as [companyId, company]}
+		{#each Object.entries(company.posts) as [postId, post]}
 			<div class="flex flex-row justify-center">
-				<Post {post} {company} {influMode} />
+				<Post {post} {company} {influMode} postId={postId} companyId={companyId}/>
 			</div>
 		{/each}
 	{/each}
