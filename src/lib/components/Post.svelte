175ebbb81e3798a43c428c2e9ec1a1b6e6f8c810
Jakub Drozd <jdrozd2003@gmail.com>
2022-12-17 13:01:34 +0100

    back to normal

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -4,19 +4,15 @@
 	const vector = '/Vector.svg';
 	export let post: any;
 	export let company: any;
-	export let influMode: boolean;
+	export let influMode: any;
 </script>
 
-<button
-	on:click={() => {
-		if (!influMode) {
-			console.log('dd');
-		}
-	}}
-	class="w-4/6  rounded-lg  border-2 shadow-md m-4 flex flex-col justify-center items-center text-black p-6 {influMode
-		? 'cursor-auto'
-		: 'cursor-pointer'}"
-	<div class="flex justify-center flex-col w-full p-6 headerPost text-white">
+<div
+	class="w-4/6 border-2 shadow-md m-4 rounded-md flex flex-col justify-center items-center text-black "
+>
+	<div
+		class="flex justify-center items-center flex-col w-full p-6 headerPost rounded-md text-white"
+	>
 		<div class="w-full flex justify-end items-center ">
 			<div class="flex justify-center items-center font-light">
 				<h1 class="text-lg mr-1">{company.localization}</h1>
@@ -34,7 +30,7 @@
 			</div>
 			<div class="flex flex-col">
 				<h1 class="text-4xl -mt-4">{post.name}</h1>
-				<h4 class="text-green-600">≈ {post.price} zł</h4>
+				<h4 style="color: #ffd24c;">≈ {post.price} zł</h4>
 			</div>
 		</div>
 	</div>
@@ -44,13 +40,26 @@
 		</div>
 		<div class="w-full flex justify-end items-center bg-white">
 			<div class="flex justify-center items-center font-extralight">
-				<Button>
-					<div class="flex flex-row">
-						<img src={vector} class="w-5 h-5 mr-1" alt="xd" />
-						Aplikuj do kampanii
-					</div>
-				</Button>
+				<h1 class="text-base mr-1 text-justify ">{post.creationDate}</h1>
 			</div>
 		</div>
-	{/if}
-</button>
+		{#if influMode}
+			<div class="w-full flex justify-end items-center bg-white">
+				<div class="flex justify-center items-center font-extralight">
+					<Button>
+						<div class="flex flex-row">
+							<img src={vector} class="w-5 h-5 mr-1" alt="xd" />
+							Aplikuj do kampanii
+						</div>
+					</Button>
+				</div>
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.headerPost {
+		background-color: #002055;
+	}
+</style>
